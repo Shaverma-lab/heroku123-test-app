c6437806c9962c9c1706348259497281a1191a78
Олег Никитин <olegn3514@gmail.com>
2022-02-17 20:25:52 +0200

    first deploy

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
+from crypt import methods
+from turtle import update
 import telebot
-from flask import Flask
+from flask import Flask, request
 from flask_sslify import SSLify
 
 
 TOKEN = '5156487975:AAEA4IaC4ivT_08mMjame_ryhOM9-AngDpI'
+APP_URL = f'https://heroku123-test-app.herokuapp.com/{TOKEN}'
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def message_for_me(message):
-    bot.send_message(message.chat.id, 'Ты написал /start')
+server = Flask(__name__)
+sslify = SSLify(server)
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hello!')
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
 
-if __name__ == '__main__':
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    
+    return '!', 200
+
+@server.route('/')
+def webhook():
     bot.remove_webhook()
-    bot.polling()
+    bot.set_webhook(url=APP_URL)
+
+    return '!', 200
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=5000)
